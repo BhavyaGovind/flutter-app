d4debe8ac35eca90c92083d2ced1459d011f3782
BhavyaGovind <bhavyagovind93@gmail.com>
2021-09-16 10:50:18 +1000

    mainactivity file issues resolved

--- android/app/src/main/kotlin/com/example/first_app/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/first_app/MainActivity.kt
@@ -1,8 +1,4 @@
 package com.example.first_app
-
-import com.microsoft.appcenter.AppCenter;
-import com.microsoft.appcenter.analytics.Analytics;
-import com.microsoft.appcenter.crashes.Crashes;
 import io.flutter.embedding.android.FlutterActivity
 
 class MainActivity: FlutterActivity() {
